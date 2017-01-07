b10705d7306aed7e23105717f88d6db571e4ca24
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-08 03:06:57 +0800

    users_controller_spec.rb #new, #create pe already passed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    # @user = User.new(name: name, email: email, password: password, password_confirmation: password_confirmation)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(User.last.id), notice: "Account created. Please log in now."
+    else
+      flash[:alert] = '"Error creating account: '
+      render new_user_path
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
