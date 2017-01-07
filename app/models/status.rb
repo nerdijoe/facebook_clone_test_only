441147a33272d64460aecf802fb4186162630b14
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-08 01:26:42 +0800

    spec/model/status_spec.rb 100%

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,13 @@
 class Status < ActiveRecord::Base
+  belongs_to :user
+  has_many :likes, :dependent => :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  validates :title, length: { minimum: 8, message: "title is too short"}
+  validates :content, length: { minimum: 10, message: "content is too short" }
+
+  default_scope -> { order(:created_at => :desc) }
+
 end
